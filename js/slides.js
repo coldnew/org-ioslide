ab7ec9af79b7e09a4b8d051f5c96cac6b5490405
kuanyui <azazabc123@gmail.com>
2015-01-17 23:57:06 +0800

    Make a fork of IO slide template.

--- js/slides.js
+++ js/slides.js
@@ -1,5 +1,5 @@
 require(['order!../slide_config', 'order!modernizr.custom.45394',
          'order!prettify/prettify', 'order!hammer', 'order!slide-controller',
-         'order!slide-deck'], function(someModule) {
+         'order!slide-deck', 'order!slide-elastic'], function(someModule) {
 
 });
