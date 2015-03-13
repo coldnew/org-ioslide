e0e146be7cadb035cb531d0bd66cfcd9604c048e
Yen-Chin Lee <coldnew.tw@gmail.com>
2015-03-13 09:21:51 +0800

    Add more info on commentary.
    melpa will render Commentary section, add more info about ox-ioslide.
    
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- ox-ioslide.el
+++ ox-ioslide.el
@@ -27,6 +27,13 @@
 
 ;;; Commentary:
 
+;;; About ox-ioslide.el
+
+;; ox-ioslide.el is an org-mode exporter to export org file to Google I/O 2013
+;; style slides.
+
+;; DEMO site: http://coldnew.github.io/org-ioslide
+
 ;;; Installation:
 
 ;; If you have `melpa` and `emacs24` installed, simply type:
